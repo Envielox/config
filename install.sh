8de97a18ae2fab8000b5a1e7a2fcc28a9a0cde52
Envielox <Envielox@gmail.com>
2017-09-02 14:51:34 +0200

    Added fonts to install script

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@ cp .zshrc ~/.zshrc
 
 cp -r .vim ~/.vim
 cp .vimrc ~/.vimrc
+
+
+wget -P ~/.vim/spell/ http://ftp.vim.org/vim/runtime/spell/en.utf-8.spl
+wget -P ~/.vim/spell/ http://ftp.vim.org/vim/runtime/spell/pl.utf-8.spl
+
+mkdir ~/.fonts
+cp SourceCodePro-powerline/* ~/.fonts/
+fc-cache -fv
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
