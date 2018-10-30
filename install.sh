349551ca4dc60e389161af0382657a5a35e66f3f
Aleksander Bułanowski <aleksander.bulanowski@ntti3.io>
2018-10-30 18:18:41 +0100

    Add comment about usefull programs

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 cp .screenrc ~/.screenrc
 cp .gitconfig ~/.gitconfig
 cp .zshrc ~/.zshrc
+# TODO instalacja vifm
 
 cp -r .vim ~/.vim
 cp .vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+# usefull programs shutter (screenshot), simple screen recorder, ncdu (ncurses du)
+# binwalk, 
