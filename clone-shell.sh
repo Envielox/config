0feb7b8052c78d7010a74244737591745f15ec38
Aleksander Bułanowski <abulanowski@google.com>
2018-11-30 12:58:19 +0100

    Better clone-shell

--- clone-shell.sh
+++ clone-shell.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
-PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
+ParentPID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
+PID=$(pgrep -P $ParentPID --list-name | grep "\(ba\|z\)sh" | cut -d " " -f 1)
 if [ -e "/proc/$PID/cwd" ]
 then
     urxvt -cd $(readlink /proc/$PID/cwd) &
